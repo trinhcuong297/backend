8e937c1ba8e6465af8f692c08a57f4dee810a17c
trinhcuong297 <trinhcuong297@gmail.com>
2022-11-11 23:12:19 +0700

    test

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const fs = require('fs');
 const hostname = '76.76.21.22';
 const port = 443;
 
-fs.readFile('index.json',(err, html) => {
+fs.readFile('index.json',(err, json) => {
 if (err) {
 	throw err;
 	}
 	const server = http.createServer((req, res) => {
 		res.statusCode  =200;
-		res.setHeader('Content-type','text/html');
-		res.write(html);
+		res.setHeader('Content-type','text/json');
+		res.write(json);
 		res.end();
 	});
 
