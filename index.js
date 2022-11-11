7dfcc4f51d216ed9b7ec4d9acccb4cd692dd2b46
trinhcuong297 <trinhcuong297@gmail.com>
2022-11-11 23:05:20 +0700

    test

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.get('/test', (req, res) => {
-res.send('Test!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '76.76.21.22';
+const port = 443;
+
+fs.readFile('index.html',(err, html) => {
+if (err) {
+	throw err;
+	}
+	const server = http.createServer((req, res) => {
+		res.statusCode  =200;
+		res.setHeader('Content-type','text/html');
+		res.write(html);
+		res.end();
+	});
+
+	server.listen(port, hostname,() => {
+		console.log("Server started at port", port);
+	});
+});
